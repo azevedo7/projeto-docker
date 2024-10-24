f82a78d662ce7fae5b5f2c17b3f70fb5c8fe688b
azevedo7 <jazevedo0406@gmail.com>
2024-10-24 15:05:46 +0100

    first with docker

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World again! Now with Docker!');
+    res.send('Hello World again! Now with Docker test 2!');
 });
 
 app.listen(port, () => {
